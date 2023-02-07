fa04ce9ff320a07216c1b04c35cc8f94dd9c8075
rBabett <richterbabett03@gmail.com>
2023-02-07 11:16:51 +0100

    search beer by name

--- searchByName.js
+++ searchByName.js
@@ -1,12 +1,15 @@
-const getBeers = require('./data')
-
+const getBeers = require("./data");
 
 function main(keyword) {
-    console.log('keyword:', keyword)
-    // Tip: string.prototype.includes()
+  const beers = getBeers();
+  console.log("keyword:", keyword);
+  // Tip: string.prototype.includes()
+  const filtered = beers.filter((beer) => beer.name.includes(keyword));
+
+  console.log(filtered);
 }
 
-main(process.argv[2])
+main(process.argv[2]);
 
 // Usage: node searchByName.js <keyword>
 // Example: node searchByName.js Rabbit
