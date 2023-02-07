21bc92d780aef629d80687a5edcffeb429a4aabf
StrangerDeer <113778183+StrangerDeer@users.noreply.github.com>
2023-02-07 11:22:08 +0100

    add filterByTypes

--- filterByType.js
+++ filterByType.js
@@ -1,11 +1,17 @@
 const getBeers = require('./data')
 
 function main(type) {
-    console.log('type:', type)
 
+    const beers = getBeers()
+
+    const typeFilter = beers.filter(beer => beer.type.includes(type))
+
+    console.log('type:', type)
+    console.log(typeFilter)
 }
 
 main(process.argv[2])
+
 
 // Usage: node filterByType.js <type>
 // Example: node filterByType.js ALE
